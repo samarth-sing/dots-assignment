b5ae347e83800cccc2f86b9957755df4a2f6eb47
samarth-sing <samarthsinghal0912@gmail.com>
2025-09-16 19:32:36 +0530

    added images

---
+++ src/assets/images/images.js
@@ -0,0 +1,17 @@
+const searchIcon = require("./searchIcon.png");
+const loader = require("./loader.gif");
+const settingIcon = require("./settings.png");
+const attachmentIcon = require("./attach-file.png");
+const peopleIcon = require("./user.png");
+const chatIcon = require("./messenger.png");
+const listIcon = require("./menu.png");
+
+export {
+  searchIcon,
+  loader,
+  settingIcon,
+  attachmentIcon,
+  peopleIcon,
+  chatIcon,
+  listIcon,
+};
